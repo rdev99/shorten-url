061c5cc4288a3023da0f5304c66ae84a222c56e4
rdev <deveshranjan920@gmail.com>
2020-11-13 21:21:50 +0530

    index.html complete and mongodb connected

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const path = require('path');
 
 const app = express();
 
@@ -9,4 +10,19 @@ app.use(bodyParser.json());
 
 const PORT = 5000 || process.env.PORT;
 
-app.listen(PORT, () => console.log(`Server Started on port : ${PORT}`) );
+const db = require('./config/keys').mongoURI;
+
+mongoose.connect(db,{ useNewUrlParser: true,useUnifiedTopology: true })
+    .then(() => console.log('MongoDB connected....'))
+    .catch(err => console.log(err));
+
+
+
+app.get("/",(req,res) => {
+    res.sendFile(path.join(__dirname,'./index.html'));
+})
+
+
+
+
+app.listen(PORT, () => console.log(`Server Started on port : ${PORT}`));
