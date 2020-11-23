a881cbe8a4220d0a05cb4163c091fdd1362e4e2a
rdev <deveshranjan920@gmail.com>
2020-11-23 16:16:47 +0530

    finished

--- server.js
+++ server.js
@@ -19,10 +19,36 @@ mongoose.connect(db,{ useNewUrlParser: true,useUnifiedTopology: true })
     .catch(err => console.log(err));
 
 
-
 app.get("/",(req,res) => {
     res.sendFile(path.join(__dirname,'./index.html'));
 })
+
+app.get("/short", async (req,res) => {
+    var obj = await ShortUrl.find();
+    res.send(obj);
+})
+
+app.post("/shorten",async (req,res) => {
+    await ShortUrl.create({fullUrl : req.body.short})
+    // console.log('Hello');
+    res.redirect('/')
+})
+
+
+app.get("/:shortt", async (req,res) => {
+    // console.log(req.params.shortt);
+    let url = await ShortUrl.findOne({shortUrl : req.params.shortt});
+    if(url==null)
+    {
+        res.sendStatus(404);
+    }
+    url.clickedNo++;
+    url.save();
+
+    res.redirect(url.fullUrl);
+})
+
+
 
 
 
